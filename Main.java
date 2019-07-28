fd2a577a269b33dbfe3f0118e127456df46fc818
Juan de Dios Hernández Dávila <juand_10193@hotmail.com>
2019-07-28 15:24:01 -0500

    Eliminamos punto y coma

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main{
 	public static void main(String[] args){
-		System.out.println("La conexion con github se ha realizado");
+		System.out.println("La conexion con github se ha realizado")
 	}
 }
